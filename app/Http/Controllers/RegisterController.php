5878576d6ec922ff170dc0dc0832a9dff7a8a8b2
Miguel Ángel García López <magarcial.dev@outlook.es>
2025-08-26 18:59:12 +0200

    Estilos del registro de usuarios y validación mínima

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    //
+    public function index() {
+        return view('auth.register');
+    }
+
+    public function store(Request $request) {
+        // dd($request);
+        $request->validate([
+            'name' => 'required|max:30',
+            'username' => 'required|unique:users|min:3|max:20',
+            'email' => 'required|unique:users|email|max:60',
+            'password' => 'required|min:8'
+        ]);
+    }
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,17 +18,17 @@
 
             <nav class="flex gap-2 items-center">
                 <a class="font-bold uppercase text-gray-600 text-sm" href="">Login</a>
-                <a class="font-bold uppercase text-gray-600 text-sm" href="">Crear cuenta</a>
+                <a class="font-bold uppercase text-gray-600 text-sm" href="{{ route('register') }}">Crear cuenta</a>
             </nav>
         </div>
     </header>
 
-    <main class="container mx-auto mt-10">
+    <main class="container mx-auto mt-10 text-center">
         <h2 class="font-black text-center text-2xl mb-10" >@yield('titulo')</h2>
         @yield('contenido')
     </main>
 
-    <footer class="text-center p-5 text-gray-500 font-bold uppercase">
+    <footer class="mt-10 text-center p-5 text-gray-500 font-bold uppercase">
         Devstagram - Todos los derechos reservados {{ now()->year }}
     </footer>
 </body>
