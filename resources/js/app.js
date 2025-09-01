9558a32178d9027c4b7850ebf634b5216e36bbc2
Miguel Ángel García <magarcial.dev@outlook.es>
2025-09-01 12:05:57 +0200

    Instalación de Dropzone

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
-import './bootstrap';
+import Dropzone from 'dropzone';
+
+window.Dropzone = Dropzone;
+
+// Desactiva la búsqueda automática de elementos con la clase 'dropzone'
+// https://docs.dropzone.dev/getting-started/setup/declarative#the-auto-discover-feature
+Dropzone.autoDiscover = false;
+
+const dropzone = new Dropzone('#dropzone', {
+    dictDefaultMessage: 'Sube aquí tu imagen',
+    acceptedFiles: '.png,.jpg,.jpeg,.gif',
+    addRemoveLinks: true,
+    dictRemoveFile: 'Eliminar archivo',
+    maxFiles: 1,
+    uploadMultiple: false,
+});
